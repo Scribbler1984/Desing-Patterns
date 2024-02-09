9b0f5c049fe9ec78378011e29db0def77fc7b28a
Egon Binder <Pushplay.EB@Googlemail.com>
2024-02-09 18:25:22 +0100

    test

---
+++ 1. MVC/1. MVC/src/MVC.java
@@ -0,0 +1,70 @@
+import java.awt.event.ActionListener;
+import javax.swing.SwingUtilities;
+import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MVC {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MVCView();
+            }
+        });
+    }
+
+}
+
+class MVCView {
+
+    JLabel label;
+
+    public MVCView() {
+        init();
+    }
+
+    private void init() {
+        label = new JLabel(" ");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        JButton button = new JButton("klick");
+        button.addActionListener(new MVCController(this));
+        JFrame frame = new JFrame("MVC");
+        frame.add(label, BorderLayout.NORTH);
+        frame.add(button, BorderLayout.CENTER);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public void setText(String s) {
+        label.setText(s);
+    }
+}
+
+class MVCController implements ActionListener {
+
+    private MVCView view;
+    private MVCModel model;
+
+    public MVCController(MVCView view) {
+        this.view = view;
+        this.model = new MVCModel();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        view.setText(model.getText());
+    }
+}
+
+class MVCModel {
+    private String text = "Hallo Welt!";
+
+    public String getText() {
+        return text;
+    }
+}
